6556efc627c22c92f9b97decb507bb15fd1140ae
ApoorvaShetty19 <38700405+ApoorvaShetty19@users.noreply.github.com>
2019-09-17 07:35:01 +0530

    Add files via upload

---
+++ ConsoleApp1/Exercise16/Exercise16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Exercise16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dirfiles = Directory.GetFiles("C:/Project1/Exercise16");
+            // there are three files in the directory abcd are :
+            // abcd.txt, simple_file.txt and xyz.txt
+
+            Console.Write("\nLINQ : Calculate the Size of File : ");
+            Console.Write("\n------------------------------------\n");
+
+            var avgFsize = dirfiles.Select(file => new FileInfo(file).Length).Average();
+            avgFsize = Math.Round(avgFsize / 10, 1);
+            Console.WriteLine("The Average file size is {0} MB", avgFsize);
+            Console.ReadLine();
+        }
+    }
+}
