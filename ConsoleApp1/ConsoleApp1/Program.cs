c71b7ca675a16bdebca3f016247c5c9076e1803a
ApoorvaShetty19 <38700405+ApoorvaShetty19@users.noreply.github.com>
2019-09-17 07:18:17 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Exercise12
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\n LINQ:Find the uppercase words in a string:");
+            Console.Write("\n-----------------------\n");
+            string strNew;
+            Console.Write("Input the string:");
+            strNew = Console.ReadLine();
+            var ucWord = WordFilt(strNew);
+            Console.Write("\n The UPPER CASE words are:\n");
+            foreach (string strRet in ucWord)
+            {
+                Console.WriteLine(strRet);
+            }
+            Console.ReadLine();
+        }
+
+        static IEnumerable<string> WordFilt(string mystr)
+        {
+            var upWord = mystr.Split(' ')
+                        .Where(x => String.Equals(x, x.ToUpper(),
+                        StringComparison.Ordinal));
+
+            return upWord;
+
+        }
+    }
+}
